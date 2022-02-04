49892a15854cfb9f00e7f7fadbf20286dc2bd304
GhaithKhoja <khojaghaith@gmail.com>
2022-02-03 21:41:30 -0500

    last push

---
+++ Driver.py
@@ -0,0 +1,75 @@
+import torch
+import numpy as np
+import random
+from dataset import get_train_val_test_loaders
+from model.CNN import CNN
+from utils import config
+import utils
+
+def main():
+    # Data loaders
+    if check_for_augmented_data("./data"):
+        tr_loader, va_loader, te_loader, _ = get_train_val_test_loaders(
+            task="target",
+            batch_size=config("challenge.batch_size"), augment = True
+        )
+    else:
+        tr_loader, va_loader, te_loader, _ = get_train_val_test_loaders(
+            task="target",
+            batch_size=config("challenge.batch_size"),
+        )
+    # Model
+    model = Challenge()
+
+    # define loss function, and optimizer
+    criterion = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001, weight_decay=0.0001)
+    #
+
+    # Attempts to restore the latest checkpoint if exists
+    print("Loading challenge...")
+    model, start_epoch, stats = restore_checkpoint(model, config("challenge.checkpoint"))
+
+    axes = utils.make_training_plot()
+
+    # Evaluate the randomly initialized model
+    evaluate_epoch(
+        axes, tr_loader, va_loader, te_loader, model, criterion, start_epoch, stats
+    )
+
+    # initial val loss for early stopping
+    prev_val_loss = stats[0][1]
+
+    #define patience for early stopping
+    patience = 10
+    curr_patience = 0
+    #
+
+    # Loop over the entire dataset multiple times
+    epoch = start_epoch
+    while curr_patience < patience:
+        # Train model
+        train_epoch(tr_loader, model, criterion, optimizer)
+
+        # Evaluate model
+        evaluate_epoch(
+            axes, tr_loader, va_loader, te_loader, model, criterion, epoch + 1, stats
+        )
+
+        # Save model parameters
+        save_checkpoint(model, epoch + 1, config("challenge.checkpoint"), stats)
+
+        # Updates early stopping parameters
+        curr_patience, prev_val_loss = early_stopping(
+            stats, curr_patience, prev_val_loss
+        )
+        #
+        epoch += 1
+    print("Finished Training")
+    # Save figure and keep plot open
+    utils.save_challenge_training_plot()
+    utils.hold_training_plot()
+
+
+if __name__ == "__main__":
+    main()
